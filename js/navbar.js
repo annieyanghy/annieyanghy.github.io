71c302783da6046b07da466b497edb314f549ff5
annie <annieyanghy@gmail.com>
2020-11-27 21:39:02 -0500

    fixing something else

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $('#resume_page').click(function(){
+    console.log("yo");
+    $('#resume_page').attr("href", "https://drive.google.com/file/d/1V-kcOQOA5KO_JezxzIZ7o68CmXif4x9R/view?usp=sharing");
+    
+  })
+  $('#blog_page').click(function(){
+    console.log("yo");
+    $('#blog_page').attr("href", "https://medium.com/@annieyanghy");    
+  })
+
+  $('#about_page').click(function(){
+    console.log("yo");
+    $('#about_page').attr("href", "about.html");    
+  })
+
+
+ 
+  
+  });
