1ba9aecf76d04a1d65c6d0b68dcf6ed63dc9f9d7
annie <annieyanghy@gmail.com>
2020-12-20 00:27:31 -0500

    footer

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,51 @@
+//nav bar
+
 $(document).ready(function() {
   $('#resume_page').click(function(){
-    console.log("yo");
+
     $('#resume_page').attr("href", "Assets/Hsin-yu Yang_Resume 2020_Fall_v1.pdf");
     
   })
   $('#blog_page').click(function(){
-    console.log("yo");
+  
     $('#blog_page').attr("href", "https://medium.com/@annieyanghy");    
   })
 
   $('#about_page').click(function(){
-    console.log("yo");
+
     $('#about_page').attr("href", "about.html");    
   })
 
+  });
+
+  //footer social
+
+  $(document).ready(function() {
+    $('#social_medium').click(function(){
+ 
+      $('#social_medium').attr("href", "https://medium.com/@annieyanghy");
+      
+    })
+    $('#social_dribbble').click(function(){
+    
+      $('#social_dribbble').attr("href", "https://dribbble.com/annieyanghy");    
+    })
+  
+    $('#social_linkedin').click(function(){
+    
+      $('#social_linkedin').attr("href", "https://www.linkedin.com/in/hsinyuyang/");    
+    })
+
+    $('#social_ig').click(function(){
+   
+      $('#social_ig').attr("href", "https://www.instagram.com/designedbyahy/");    
+    })
+
+    $('social_email').click(function(){
+   
+      $('social_email').attr("href", "mailto:hsinyuy@umich.edu");    
+    })
+  
+    });
 
  
-  
-  });
