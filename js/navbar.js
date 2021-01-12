02d71aa6b3cddd207d5b996fa041e5db721be965
annie <annieyanghy@gmail.com>
2021-01-12 13:09:36 -0500

    resume 2021 Winter v1

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
   $('#resume_page').click(function(){
 
-    $('#resume_page').attr("href", "Assets/Hsin-yu Yang_Resume 2020_Fall_V2.2.pdf");
+    $('#resume_page').attr("href", "Assets/Hsin-Yu Yang_Resume_2021 Winter_V1.pdf");
     
   })
   $('#blog_page').click(function(){
